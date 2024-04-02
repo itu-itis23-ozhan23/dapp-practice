2292859f4ca3c864a95e075f873430eba139eb99
Elif Işıl Özhan <ozhan23@itu.edu.tr>
2024-04-02 13:08:34 +0300

    even number  easy solved. easy  - Apıdon-103

---
+++ even_number.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EvenOddContract {
+  
+    function isEven(uint256 number) external pure returns (bool) {
+     return number % 2 == 0; 
+    }
+}
+
